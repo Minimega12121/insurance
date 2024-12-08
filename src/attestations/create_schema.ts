08d3fa9a1bd396c8d02ef668a8c8cafe12388b9e
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-12-08 07:48:58 +0530

    correct attestations create

--- src/attestations/create_schema.ts
+++ src/attestations/create_schema.ts
@@ -1,7 +1,7 @@
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { ethers } from 'ethers';
 
-export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
+export const EASContractAddress = "0x4200000000000000000000000000000000000021"; // Sepolia v0.26
 
 // Gets a default provider (in production use something else like infura/alchemy)
 // const provider = new ethers.JsonRpcProvider(process.env.QUICKNODE);
@@ -9,11 +9,18 @@ export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; 
 // // const privateKey = process.env.PRIVATE_KEY || "";
 // const privateKey = "930801543d439570fa5dd164932ce48f1df7a0bffa566344c2c11d720f5dd54a";
 
-const sample_user = "0xc6CD7CdFa6500F63e669930e30ED32BBEC9890eC".toLowerCase();
+// const sample_user = "0x215a58eEF2ae37478C461ca44B9329F261484a9c".toLowerCase();
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+// const privateKey = process.env.PRIVATE_KEY || "";
+const privateKey = "0xabf9989497966f655d7cb397f1e3868259c060a3a7920c20f3a563392fb9cb3b";
+
+// Initialize SchemaEncoder with the schema string
+const user_address="0x215a58eEF2ae37478C461ca44B9329F261484a9c".toLowerCase();
+
 const schemaUID = "0x779d51ae429a271b4384453f955b4620375cbc8e727b43c1f4306aab9409038f";
 
 // Attest the data
-export async function attestData(signer: ethers.Signer, hosptial_data: string) {
+export async function attestData(_signer: ethers.Signer, hospital_data: string) {
   // if (!window.ethereum) {
   //   throw new Error("No Web3 provider found. Please install MetaMask.");
   // }
@@ -22,20 +29,23 @@ export async function attestData(signer: ethers.Signer, hosptial_data: string) {
   // const signer = provider.getSigner();
   // // console.log(signer._address);
   // // // Initialize EAS with the custom signer
-  const eas = new EAS(EASContractAddress);
-  eas.connect(signer);
+  const signer1 = new ethers.Wallet(privateKey, provider);
 
-  const schemaEncoder = new SchemaEncoder("string hosptial, address user");
+  const eas = new EAS(EASContractAddress);
+  eas.connect(signer1);
+
+  const schemaEncoder = new SchemaEncoder("string hospital, address user");
 
   const encodedData = schemaEncoder.encodeData([
-    { name: "hosptial", value: hosptial_data, type: "string" },
-    { name: "user", value: sample_user, type: "address" },
+    { name: "hospital", value: hospital_data, type: "string" },
+    { name: "user", value: user_address, type: "address" },
   ]);
+  eas.connect(signer1);
 
   const tx = await eas.attest({
     schema: schemaUID,
     data: {
-      recipient: sample_user,   
+      recipient: user_address,   
       revocable: false,
       data: encodedData,
     },
